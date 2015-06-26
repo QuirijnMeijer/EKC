0095335505696f5c16dbf5401849cfd504a58f5b
QuirijnMeijer <Quir@live.nl>
2015-06-26 23:35:09 +0200

    Enkele aanpassingen
    Fotmarge toegevoegd aan de Elliptische Kromme over R zodat
    afrondingsfouten het programma niet langer verstoren. Diffie-Hellman
    werkend gekregen.

--- Implementatie/Uitvoering.py
+++ Implementatie/Uitvoering.py
@@ -14,3 +14,20 @@ from ElliptischeKromme import *
 from DiffieHellman import *
 
 # <Uit te voeren code>
+
+# ~ Onderstaand moet nog zo verwerkt worden dat het werkt in de command line.
+
+# ~~~>
+K = ElliptischeKromme(0, 17, 0)
+P = Punt(K, -1, 4)
+DH = DiffieHellman(K, P)
+a = int(input('λ? '))
+PublicKeyA = DH.publiekeSleutel(a)
+print('De publieke sleutel van persoon A is %s' % PublicKeyA)
+b = int(input('μ? '))
+PublicKeyB = DH.publiekeSleutel(b)
+print('De publieke sleutel van persoon B is %s' % PublicKeyB)
+SharedKeyA = PublicKeyB**a
+SharedKeyB = PublicKeyA**b
+print('De geheime sleutel is [A] %s == %s [B]' % (SharedKeyA, SharedKeyB))
+# <~~~
