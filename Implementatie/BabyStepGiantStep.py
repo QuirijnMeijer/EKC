48a04b6d30174bba720fab63d75e9790dd4e5c7b
QuirijnMeijer <Quir@live.nl>
2015-07-01 11:35:22 +0200

    BSGS toegevoegd
    En het werkt!

--- Implementatie/BabyStepGiantStep.py
+++ Implementatie/BabyStepGiantStep.py
@@ -7,16 +7,17 @@ z = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(z)
 # <---
 
-# Bundeling van alles benodigd voor een poging het discreet logaritmisch probleem te breken.
+# Bundeling van methoden benodigd voor een poging het discreet logaritmisch probleem te breken.
 from Paar import *
 import math
 
 class BabyStepGiantStep(object):
-    """Algoritme om het discreet logaritmisch probleem in een groep gegenereerd door het punt k op de elliptische kromme K te breken"""
+    """Algoritme om het discreet logaritmisch probleem in een groep gegenereerd door het punt k op de elliptische kromme K te breken. Stel m=0 wanneer de |K(fp)| onbekend is."""
 
     __slots__ = ['K', 'k', 'm', 'D', 'Z', 'tabel']
 
     def __init__(self, K, k, m):
+        assert K.p > 0, 'De opgegeven elliptische kromme is ongeldig.'
         self.K = K
         self.k = k
         self.m = m
@@ -24,19 +25,26 @@ class BabyStepGiantStep(object):
             self.m = math.ceil(2 * math.sqrt(K.p) + K.p + 1)
         self.reset()
 
+    def __str__(self):
+        return 'Actief: de groep [%s, +] met genererend element %s en doel %s.' % (self.K, self.k, self.D)
+
     # Herstelt de gehele instantie naar standaardwaarden
     def reset(self):
         self.tabel = []
-        self.D = 'O'
-        self.Z = self.D
+        self.verstelDoel('O')
         self.vulTabel()
 
-    # Vult de tabel met elementen jk
+    # Vult de tabel met elementen k^j
     def vulTabel(self):
-        j = 0
+        self.tabel.append(Paar(0, 'O'))
+        j = 1
         while j < self.m:
             self.tabel.append(Paar(j, self.k**j))
             j += 1
+
+    # Verkrijg het element op index j in de tabel
+    def verkrijgElement(self, j):
+        return self.tabel[j].aP
 
     # Verstelt het element waar naar gezocht wordt
     def verstelDoel(self, D):
@@ -45,16 +53,15 @@ class BabyStepGiantStep(object):
 
     # Doorzoekt de tabel en vergelijkt alle elementen om ν te bepalen
     def vindMacht(self):
-        i = 0
+        i = 1
+        increment = self.K.negatie(self.k)**self.m
         while i < self.m:
-            self.Z += self.K.negatie(self.k)
+            self.Z += increment
             j = 1
             while j < self.m:
-                if not self.verkrijgElement(i) == 'O' and not self.Z == 'O' and self.Z.isGelijk(self.verkrijgElement(i)):
+                if not (self.verkrijgElement(j) == 'O' and self.Z == 'O') and self.Z.isGelijk(self.verkrijgElement(j)):
+                    return i*self.m+j
+                elif self.verkrijgElement(j) == 'O' and self.Z == 'O':
                     return i*self.m+j
                 j += 1
             i += 1
-
-    # Verkrijg het element op index j in de tabel
-    def verkrijgElement(self, j):
-        return self.tabel[j].aP
--- Implementatie/Uitvoering_Baby_Step_Giant_Step.py
+++ Implementatie/Uitvoering_Baby_Step_Giant_Step.py
@@ -15,7 +15,14 @@ from BabyStepGiantStep import *
 # Start van het script
 
 K = ElliptischeKromme(2, 2, 17)
-P = Punt(K, 0, 6)
-Z = P**6
-B = BabyStepGiantStep(K, P, 0)
-B.verstelDoel(Z)
+k = Punt(K, 0, 6)
+BSGS = BabyStepGiantStep(K, k, 0)
+print(BSGS)
+x = int(input('Geef het x-coördinaat van het doel: '))
+y = int(input('Geef het y-coördinaat van het doel: '))
+D = Punt(K, x, y)
+assert K.verifieer(D), 'Het opgegeven punt is niet compatibel.'
+BSGS.verstelDoel(D)
+c = BSGS.vindMacht()
+P = K.vermenigvuldig(k, c)
+print('De gevonden waarde van ν is %d. Opgegeven is het punt %s. Er geldt ν%s = %s.' % (c, D, k, P))
